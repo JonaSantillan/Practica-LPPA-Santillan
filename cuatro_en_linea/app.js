b760f8efd972bcdd0b2a933f3cc4ad111ee4d035
Jonatan Santillan <jonatansantillan@gmail.com>
2020-06-13 14:18:46 -0300

    4enlinea

--- cuatro_en_linea/app.js
+++ cuatro_en_linea/app.js
@@ -7,31 +7,32 @@ var estado_tablero = [
     [0,0,0,0,0,0,0],
   ]
 
-  var turno = 1
+var turno = 1
   
-  window.onload = function() {
-    estado_tablero.forEach(function(fila, y) {
+window.onload = function() {
+   estado_tablero.forEach(function(fila, y) {
+     fila.forEach(function(valor, x) {
+       var celda = document.getElementById(x + '-' + y)
+       celda.onclick = (function(){
+           if(estado_tablero[y][x] === 0){
+                estado_tablero[y][x] = turno
+                turno = turno === 1 ? 2 : 1
+                pintar()
+            }
+       })
+     })
+   })
+ }
+
+var pintar = function() {
+  estado_tablero.forEach(function(fila, y) {
       fila.forEach(function(valor, x) {
         var celda = document.getElementById(x + '-' + y)
-        celda.onclick = (function(){
-            estado_tablero[y][x] = turno
-            turno = turno === 1 ? 2 : 1
-            pintar()
-        })
+        if (valor === 1){
+            celda.className = "celdaJ1"
+        }else if (valor === 2){
+            celda.className = "celdaJ2"
+        }
       })
-    })
-    pintar()
-  }
-  
-  var pintar = function() {
-    estado_tablero.forEach(function(fila, y) {
-        fila.forEach(function(valor, x) {
-          var celda = document.getElementById(x + '-' + y)
-          if (valor === 1){
-              celda.className = "celdaJ1"
-          }else if (valor === 2){
-              celda.className = "celdaJ2"
-          }       
-        })
-    })
-  }
+  })
+}
