7143fd578a20b1909a644cf912e2998ea6595299
Jonatan Santillan <jonatansantillan@gmail.com>
2020-06-20 19:18:31 -0300

    preliminar

--- cuatro_en_linea/app.js
+++ cuatro_en_linea/app.js
@@ -14,26 +14,109 @@ window.onload = function() {
    estado_tablero.forEach(function(fila, y) {
      fila.forEach(function(valor, x) {
        var celda = document.getElementById(x + '-' + y)
-       var cambio1 = document.getElementById("Jugador1")
-       var cambio2 = document.getElementById("Jugador2")
+       var cambio1 = document.getElementById("Jugador")
        celda.onclick = (function(){
-           if(estado_tablero[y][x] === 0){
-                estado_tablero[y][x] = turno
-                turno = turno === 1 ? 2 : 1
-                pintar()
-                if(turno === 1){
-                    cambio1.className = "activo1"
-                    cambio2.className = "activo"
-                }
-                if(turno === 2){
-                    cambio1.className = "activo"
-                    cambio2.className = "activo2"
-                }
-            }
-       })
+           for (let y = 0; y < 6; y++) {
+             const valor = estado_tablero[y][x];
+             if(valor !== 0){
+               estado_tablero[y - 1][x] = turno
+               turno = turno === 1 ? -1 : 1
+               break
+             }else if (y === 5){
+              estado_tablero[y][x] = turno
+              turno = turno === 1 ? -1 : 1
+             }
+           }
+          pintar()
+          chequearGanador()
+          if(turno === 1){
+              cambiar()
+              cambio1.className = "activo1"
+          }
+          if(turno === -1){
+              cambiar()
+              cambio1.className = "activo2"
+          }
+
+        })
+
+        celda.onmouseover = mousearriba(x)
+        celda.onmouseout = mousenoarriba(x)
+
      })
    })
 }
+
+var reset = function(){
+  var ptoj1 = 1
+  var ptoj2 = 1
+  document.getElementById("ptojg1").innerHTML= "Jugador 1: " + ptoj1 + " Puntos";
+  document.getElementById("ptojg2").innerHTML= "Jugador 2: " + ptoj2 + " Puntos";
+
+
+}
+
+
+var chequearGanador = function(){
+  for (let y = 0; y < 6; y++) {
+    for (let x = 0; x < 7; x++) {
+      try{
+        var celdax1 = estado_tablero[y][x]
+        var celdax2 = estado_tablero[y][x + 1]
+        var celdax3 = estado_tablero[y][x + 2]
+        var celdax4 = estado_tablero[y][x + 3]
+
+        var celday1 = estado_tablero[y][x]
+        var celday2 = estado_tablero[(y + 1)] && estado_tablero[(y + 1)][x]
+        var celday3 = estado_tablero[(y + 2)] && estado_tablero[(y + 2)][x]
+        var celday4 = estado_tablero[(y + 3)] && estado_tablero[(y + 3)][x]
+
+        var resultadox = (celdax1 + celdax2 + celdax3 + celdax4)
+        var resultadoy = (celday1 + celday2 + celday3 + celday4)
+
+        if(Math.abs(resultadox) === 4 || Math.abs(resultadoy) === 4){
+          var jugadorverde = 2
+          var jugadorrojo = 1
+          if(turno === 1){
+            alert("Ganador: Jugador  " + jugadorverde)
+          }
+          if(turno === -1){
+            alert("Ganador: Jugador  " + jugadorrojo)
+          }
+        }
+      }
+      catch(error){
+        //vacio
+      }
+  }
+}
+}
+
+var mousearriba = function (x){
+  return function(){
+    var triang = document.getElementById("trian_" + x)
+    triang.style.borderTop = "30px solid #ffee00"
+  }
+}
+
+var mousenoarriba = function (x){
+  return function(){
+    var triang = document.getElementById("trian_" + x)
+    triang.style.borderTop = "30px solid transparent"
+  }
+}
+
+function cambiar() {
+  var jugadorturno = 2
+  if(turno === 1){
+    document.getElementById("Jugador").innerHTML= "JUGADOR " + turno ;
+  }
+  if(turno === -1){
+    document.getElementById("Jugador").innerHTML= "JUGADOR " + jugadorturno;
+  }
+
+}
+
 
 var pintar = function() {
   estado_tablero.forEach(function(fila, y) {
@@ -41,10 +124,9 @@ var pintar = function() {
         var celda = document.getElementById(x + '-' + y)
         if (valor === 1){
             celda.className = "celdaJ1"
-        }else if (valor === 2){
+        }else if (valor === -1){
             celda.className = "celdaJ2"
         }
       })
   })
 }
-
