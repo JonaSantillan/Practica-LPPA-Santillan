8578bbde0053e7baa5fc0be9d2f05de63b25d7c5
Jonatan Santillan <jonatansantillan@gmail.com>
2020-07-12 13:31:34 -0300

    V. PreFinal

--- cuatro_en_linea/app.js
+++ cuatro_en_linea/app.js
@@ -8,66 +8,73 @@ var estado_tablero = [
     [0,0,0,0,0,0,0],
   ]
 
+var estado_inicial = [
+  [0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0],
+]
+
 
 var turno = 1
 
 
-  window.onload = function() {
-    estado_tablero.forEach(function(fila, y) {
-      fila.forEach(function(valor, x) {
-        var celda = document.getElementById(x + '-' + y)
-        var cambio1 = document.getElementById("Jugador")
-        celda.onclick = (function(){
-            for (let y = 0; y < 6; y++) {
-              const valor = estado_tablero[y][x];
-              if(valor !== 0){
-                estado_tablero[y - 1][x] = turno
-                turno = turno === 1 ? -1 : 1
-                break
-              }else if (y === 5){
-                estado_tablero[y][x] = turno
-                turno = turno === 1 ? -1 : 1
-              }
-            }
-            pintar()
-            chequearGanador()
-            if(turno === 1){
-                cambiar()
-                cambio1.className = "activo1"
-            }
-            if(turno === -1){
-                cambiar()
-                cambio1.className = "activo2"
-            }
+window.onload = function() {
+ //colocar para que reestablezca el tablero al refrescar
+  if(localStorage.getItem("estado") !== null){
+    estado_tablero = JSON.parse(localStorage.getItem("estado"))
+    pintar()
+  }
 
-          })
-          celda.onmouseover = mousearriba(x)
-          celda.onmouseout = mousenoarriba(x)
-          Guardar()
-          Cargar()
+  estado_tablero.forEach(function(fila, y) {
+    fila.forEach(function(valor, x) {
+      var celda = document.getElementById(x + '-' + y)
+      var cambio1 = document.getElementById("Jugador")
+      celda.onclick = (function(){
+        for (let y = 0; y < 6; y++) {
+          const valor = estado_tablero[y][x];
+          if(valor !== 0){
+            estado_tablero[y - 1][x] = turno
+            turno = turno === 1 ? -1 : 1
+            break
+          }else if (y === 5){
+            estado_tablero[y][x] = turno
+            turno = turno === 1 ? -1 : 1
+          }
+        }
+        pintar()
+        chequearGanador()
+          if(turno === 1){
+            cambiar()
+            cambio1.className = "activo1"
+          }
+          if(turno === -1){
+            cambiar()
+            cambio1.className = "activo2"
+          }
+        localStorage.setItem("estado", JSON.stringify(estado_tablero))
       })
+      celda.onmouseover = mousearriba(x)
+      celda.onmouseout = mousenoarriba(x)
+      //Guardar()
+      //Cargar()
     })
-  }
-
-
-
-
-var reset = function(){
-  var mensaje = confirm("¿Jugamos otra partida?")
-  if(mensaje){
-    alert("Juguemos otra vez!")
-    location.reload()
-  }else{
-    alert("Nos vemos otro día!")
-    window.close()
-  }
+  })
+  reset()
 }
 
 
 var chequearGanador = function(){
+  var cero = false
   for (let y = 0; y < 6; y++) {
     for (let x = 0; x < 7; x++) {
       try{
+        if(estado_tablero[y][x] === 0){
+          cero = true
+        }
+
         var celdax1 = estado_tablero[y][x]
         var celdax2 = estado_tablero[y][x + 1]
         var celdax3 = estado_tablero[y][x + 2]
@@ -86,19 +93,20 @@ var chequearGanador = function(){
           var jugadorrojo = 1
           if(turno === 1){
             alert("Ganador: Jugador  " + jugadorverde)
-            reset()
           }
           if(turno === -1){
             alert("Ganador: Jugador  " + jugadorrojo)
-            reset()
           }
         }
       }
       catch(error){
         //vacio
       }
+    }
+  } 
+  if(!cero){
+    alert("Hubo un empate")
   }
-}
 }
 
 var mousearriba = function (x){
@@ -126,20 +134,53 @@ function cambiar() {
 
 }
 
-
 var pintar = function() {
   estado_tablero.forEach(function(fila, y) {
-      fila.forEach(function(valor, x) {
-        var celda = document.getElementById(x + '-' + y)
-        if (valor === 1){
-            celda.className = "celdaJ1"
-        }else if (valor === -1){
-            celda.className = "celdaJ2"
-        }
-      })
+    fila.forEach(function(valor, x) {
+      var celda = document.getElementById(x + '-' + y)
+      if (valor === 1){
+          celda.className = "celdaJ1"
+      }else if (valor === -1){
+          celda.className = "celdaJ2"
+      }
+    })
   })
 }
 
+
+var reset = function(){
+  var reset = document.getElementById("reset")
+  reset.onclick = function(){  
+    var estado_reset = [
+      [0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0],
+    ]
+    despintar()
+    localStorage.removeItem("estado")
+    JSON.stringify(localStorage.setItem("estado", estado_reset))
+    estado_tablero = estado_reset
+  }
+
+}
+
+var despintar = function() {
+  estado_tablero.forEach(function(fila, y) {
+    fila.forEach(function(valor, x) {
+      var celda = document.getElementById(x + '-' + y)
+      if (valor === 1){
+          celda.className = "celda"
+      }else if (valor === -1){
+          celda.className = "celda"
+      }
+    })
+  })
+}
+
+/*
 var Guardar = function(){
   var guardar = document.getElementById("guardar_partida")
   guardar.onclick = function(){
@@ -153,3 +194,4 @@ var Cargar = function(){
     localStorage.getItem("tablero")
   }
 }
+*/
