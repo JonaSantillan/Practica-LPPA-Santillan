697fd25e005673015754f16d3f6cbb66b962e99a
Jonatan Santillan <jonatansantillan@gmail.com>
2020-08-05 17:58:06 -0300

    Version pre-final

--- cuatro_en_linea/app.js
+++ cuatro_en_linea/app.js
@@ -9,17 +9,15 @@ var estado_tablero = [
   ]
 
 var estado_inicial = [
-  [0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0],
-]
-
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+  ]
 
 var turno = 1
-
 
 window.onload = function() {
  //colocar para que reestablezca el tablero al refrescar
@@ -27,7 +25,6 @@ window.onload = function() {
     estado_tablero = JSON.parse(localStorage.getItem("estado"))
     pintar()
   }
-
   estado_tablero.forEach(function(fila, y) {
     fila.forEach(function(valor, x) {
       var celda = document.getElementById(x + '-' + y)
@@ -49,22 +46,78 @@ window.onload = function() {
           if(turno === 1){
             cambiar()
             cambio1.className = "activo1"
+            Detenerse2()
+            TiempoJug1()
           }
           if(turno === -1){
             cambiar()
             cambio1.className = "activo2"
+            Detenerse1()
+            TiempoJug2()
           }
         localStorage.setItem("estado", JSON.stringify(estado_tablero))
       })
       celda.onmouseover = mousearriba(x)
       celda.onmouseout = mousenoarriba(x)
-      //Guardar()
-      //Cargar()
     })
   })
+  TiempoJug1()
   reset()
+  Guardar()
 }
 
+//Contador de Tiempo
+
+var TiempoJug1 = function(){
+  var n = 0
+  var l1 = document.getElementById("SegJ1") 
+  n = l1.innerHTML
+
+  tiempo1 = window.setInterval(function(){
+    l1.innerHTML = n
+    n++
+  },1000)
+}
+
+var TiempoJug2 = function(){
+  var n = 0
+  var l2 = document.getElementById("SegJ2")
+  n = l2.innerHTML
+
+  tiempo2 = window.setInterval(function(){
+    l2.innerHTML = n
+    n++
+  },1000)
+}
+
+var Detenerse1 = function(){
+  clearInterval(tiempo1)
+}
+
+var Detenerse2 = function(){
+  clearInterval(tiempo2)
+}
+
+//fin contador de tiempos
+
+//Indicadores
+var mousearriba = function (x){
+  return function(){
+    var triang = document.getElementById("trian_" + x)
+    triang.style.borderTop = "30px solid #ffee00"
+  }
+}
+
+var mousenoarriba = function (x){
+  return function(){
+    var triang = document.getElementById("trian_" + x)
+    triang.style.borderTop = "30px solid transparent"
+  }
+}
+
+//Fin indicadores
+
+//Operaciones 
 
 var chequearGanador = function(){
   var cero = false
@@ -92,10 +145,12 @@ var chequearGanador = function(){
           var jugadorverde = 2
           var jugadorrojo = 1
           if(turno === 1){
-            alert("Ganador: Jugador  " + jugadorverde)
+            alert("Ganador: Jugador " + jugadorverde)
+            ResetManual()
           }
           if(turno === -1){
             alert("Ganador: Jugador  " + jugadorrojo)
+            ResetManual()
           }
         }
       }
@@ -106,20 +161,7 @@ var chequearGanador = function(){
   } 
   if(!cero){
     alert("Hubo un empate")
-  }
-}
-
-var mousearriba = function (x){
-  return function(){
-    var triang = document.getElementById("trian_" + x)
-    triang.style.borderTop = "30px solid #ffee00"
-  }
-}
-
-var mousenoarriba = function (x){
-  return function(){
-    var triang = document.getElementById("trian_" + x)
-    triang.style.borderTop = "30px solid transparent"
+    ResetManual()
   }
 }
 
@@ -131,7 +173,6 @@ function cambiar() {
   if(turno === -1){
     document.getElementById("Jugador").innerHTML= "JUGADOR " + jugadorturno;
   }
-
 }
 
 var pintar = function() {
@@ -147,6 +188,39 @@ var pintar = function() {
   })
 }
 
+var despintar = function() {
+  estado_tablero.forEach(function(fila, y) {
+    fila.forEach(function(valor, x) {
+      var celda = document.getElementById(x + '-' + y)
+      if (valor === 1){
+          celda.className = "celda"
+      }else if (valor === -1){
+          celda.className = "celda"
+      }
+    })
+  })
+}
+
+var ResetManual = function(){
+  var estado_reset = [
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+  ]
+  despintar()
+  localStorage.removeItem("estado")
+  JSON.stringify(localStorage.setItem("estado", estado_reset))
+  estado_tablero = estado_reset
+  var time1 = document.getElementById("SegJ1")
+  var time2 = document.getElementById("SegJ2")
+  Detenerse1()
+  Detenerse2()
+  time1.innerHTML = 0
+  time2.innerHTML = 0
+}
 
 var reset = function(){
   var reset = document.getElementById("reset")
@@ -163,35 +237,110 @@ var reset = function(){
     localStorage.removeItem("estado")
     JSON.stringify(localStorage.setItem("estado", estado_reset))
     estado_tablero = estado_reset
+    
+    var time1 = document.getElementById("SegJ1")
+    var time2 = document.getElementById("SegJ2")
+    Detenerse1()
+    Detenerse2()
+    time1.innerHTML = 0
+    time2.innerHTML = 0
   }
+}
+
+//Guarda Partida en Tabla
+
+/*La Partida guarda lo siguiente:
+- id de partida
+- fecha
+- tiempo j1
+- tiempo j2
+- seleccionar 
+
+
+id="idpartida"
+id="fechapartida"
+id="timeJ1"
+id="timeJ2"
+id="select"
+*/
+
+var ListaPartida = []
+
+var idp = document.getElementById("idpartida")
+var fechap = document.getElementById("fechapartida")
+var tJ1 = document.getElementById("timeJ1")
+var tJ2 = document.getElementById("timeJ2")
+var select = document.getElementById("select")
+var estate
+
+//esta funcion debe agregar una partida
+var AgregarPartida = function (idp, fechap, tJ1, tJ2, select, estate){
+//ver id de como ponerlo....
+  if(idp){
+    idp = idp + 1
+  }
+
+  var time1 = document.getElementById("SegJ1")
+  var time2 = document.getElementById("SegJ2")
+
+  fechap = new Date()
+
+  tJ1 = time1.innerHTML
+  tJ2 = time2.innerHTML
+  estate = JSON.parse(localStorage.getItem("estado"))
+
+
+  var newPartida = {
+    id : idp,
+    Fecha : fechap,
+    tiempoj1 : tJ1,
+    tiempoj2 : tJ2,
+    Selector : select,
+    estadoG : estate
+  }
+
+
+
+  ListaPartida.push(newPartida)
 
 }
 
-var despintar = function() {
-  estado_tablero.forEach(function(fila, y) {
-    fila.forEach(function(valor, x) {
-      var celda = document.getElementById(x + '-' + y)
-      if (valor === 1){
-          celda.className = "celda"
-      }else if (valor === -1){
-          celda.className = "celda"
-      }
-    })
+var Guardar = function(){
+  var guardar = document.getElementById("guardar")
+  guardar.onclick = function(){
+    AgregarPartida()
+    localStorage.setItem("Partida", JSON.stringify(ListaPartida)) //estoy guardando lo que ya tenia guardado
+    ArmarTabla(ListaPartida)
+  }
+}
+
+var ArmarTabla = function(ListaPartida){
+  var idp = document.getElementById("idpartida")
+  var fechap = document.getElementById("fechapartida")
+  var tJ1 = document.getElementById("timeJ1")
+  var tJ2 = document.getElementById("timeJ2")
+  var select = document.getElementById("select")
+
+  ListaPartida.forEach(function(partida){
+    var li = document.createElement("li")
+    li.appendChild(document.createTextNode(partida.id))
+    idp.appendChild(li)
+    console.log(idp)
+
+    var li = document.createElement("li")
+    li.appendChild(document.createTextNode(partida.Fecha))
+    fechap.appendChild(li)
+
+    var li = document.createElement("li")
+    li.appendChild(document.createTextNode(partida.tiempoj1))
+    tJ1.appendChild(li)
+
+    var li = document.createElement("li")
+    li.appendChild(document.createTextNode(partida.tiempoj2))
+    tJ2.appendChild(li)
+
+
   })
 }
 
-/*
-var Guardar = function(){
-  var guardar = document.getElementById("guardar_partida")
-  guardar.onclick = function(){
-    localStorage.setItem("tablero", estado_tablero)
-  }
-}
-
-var Cargar = function(){
-  var cargar = document.getElementById("cargar_partida")
-  cargar.onclick = function(){
-    localStorage.getItem("tablero")
-  }
-}
-*/
+//Fin Operaciones
